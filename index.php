e0d01987686d452fad0876449c3642710c872c45
L0LZ666H05T <36075615+l0lz666h05t@users.noreply.github.com>
2019-04-27 17:17:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<center><h1>HACKED BY L0LZ666H05T</h1></center>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<style type="text/css">
+	body {
+		background: black;
+		color: lavender;
+		text-shadow: 2px 2px 4px #000000;
+		background: url(https://ak2.picdn.net/shutterstock/videos/18876602/thumb/1.jpg?i10c=img.resize(height:160)) no-repeat center center fixed;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+	}
+	@font-face {
+    font-family: Hacked;
+    src: url(https://hackedfont.com/HACKED.ttf);
+   } h1 {
+   	font-family: Hacked;
+   	font-size:70px;
+   }
+</style>
+<body>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<center>
+	<h1>HACKED BY L0LZ666H05T</h1>
+</center>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/green-bintang-jatuh.js" type="text/javascript"></script>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+</body>
+</html>
