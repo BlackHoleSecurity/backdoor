b4ca7c3cf5a8c13a21c1021d562927bf41fbdfb5
Cvar1984 <32727560+Cvar1984@users.noreply.github.com>
2019-12-01 16:12:22 +0700

    Add files via upload

--- make.php
+++ make.php
@@ -1,15 +1,19 @@
 #!/usr/bin/env php
 <?php
-$file=readline("input filename : ");
 try {
-    $phar = new Phar('main.phar');
-    $phar['index.php'] = file_get_contents($file);
+    $phar = new Phar('ini.phar');
+    $input = readline('count of files to compress : ');
+    $file = array();
+    for ($x = 0; $x < $input; $x++) {
+        $file[$x] = trim(readline('filename : '));
+        $phar[basename($file[$x])] = file_get_contents($file[$x]);
+    }
     $phar->compressFiles(Phar::BZ2);
     $phar->setSignatureAlgorithm(Phar::SHA1, sha1('Cvar1984'));
     $phar->setStub('<?php
 Phar::webPhar();
 __HALT_COMPILER(); ?>');
 } catch (Exception $e) {
-    // handle error here
+    echo $e->getMessage();
 }
 
